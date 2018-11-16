96a60dd96b03f28c325f16a29655981b731526a4
ayakatakuriko <warangkana0601@yahoo.ne.jp>
2018-11-16 22:27:30 +0900

    fix

--- udpsrv.c
+++ udpsrv.c
@@ -1,44 +1,29 @@
 /**
    udpcil.c
-   @brief あるIPアドレスをもつPCにUDPでメッセージを送信して、UDPでメッセージを受信するプログラム。
+   @brief PCにUDPでメッセージを送信して、UDPでメッセージを受信するプログラム。
    @author ayakatakuriko
    @date 2018/11/16
  */
 
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <netinet/in.h>
 #include <string.h>
-
-#define CLI_IP_ADDR "131.113.108.53" // Client IP address
+#include <arpa/inet.h>
+#include <netdb.h>
 
 int main() {
         int s, count, datalen;
         in_port_t myport, port;
         struct sockaddr_in myskt, skt;
         char sbuf[512], buf[512], c;
-        int position;
         socklen_t sktlen;
 
         myport = 12345;
         port = 12345;
-
-        /*Input data.*/
-        position = 0;
-        while ((c = getchar()) != EOF) {
-                if (c == '\n') continue;
-                else {
-                        sbuf[position++] = c;
-                }
-        }
-
-        datalen = position;
 
         if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
                 perror("socket");
@@ -54,15 +39,6 @@ int main() {
                 exit(1);
         }
 
-        memset(&skt, 0, sizeof skt);
-        skt.sin_family = AF_INET;
-        skt.sin_port = htons(port);
-        skt.sin_addr.s_addr = inet_addr(CLI_IP_ADDR);
-        if ((count = sendto(s, sbuf, datalen, 0,
-                            (struct sockaddr *)&skt, sizeof skt)) < 0) {
-                perror("sendto");
-                exit(1);
-        }
 
         sktlen = sizeof skt;
         memset(buf, 0, sizeof(buf));
@@ -73,7 +49,14 @@ int main() {
         }
         printf("%s: %s\n", inet_ntoa(skt.sin_addr), buf);
 
+        if ((count = sendto(s, buf, datalen, 0,
+                            (struct sockaddr *)&skt, sizeof skt)) < 0) {
+                perror("sendto");
+                exit(1);
+        }
+
         close(sock);
 
         return 0;
+
 }
