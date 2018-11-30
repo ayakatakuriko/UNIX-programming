4f5db3bf442454975ed86b5e0aea0a9e3af1862e
ua211110 <ua211110@y12a027.educ.cc.keio.ac.jp>
2018-11-30 10:11:48 +0900

    fix

--- udpcil.c
+++ udpcil.c
@@ -1,67 +1,106 @@
 /**
-udpcil.c
-@brief PCにUDPでメッセージを送信して、UDPでメッセージを受信するプログラム。
-@author ayakatakuriko
-@date 2018/11/16
-*/
+   udpcil.c
+   @brief あるIPアドレスをもつPCにUDPでメッセージを送信して、UDPでメッセージを受信するプログラム。
+   @author ayakatakuriko
+   @date 2018/11/16
+ */
 
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+/*
+   netinet/in.h: Defines the in_addr structure and the sockaddr_in structure.
+   Contain definition of Internet protocol family
+ */
+#include <netinet/in.h>
+/*
+   arpa/inet.h: Makes available the type in_port_t, the type in_addr_t and the
+   in_addr structure.
+   Contain definition to handle Internet.
+ */
+#include <arpa/inet.h>
+#include <netdb.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <netinet/in.h>
 #include <string.h>
 
+#define CLI_IP_ADDR "131.113.108.53" /* IP address to send data*/
+#define BUF_SIZE 512
+
 int main() {
-	int s, count, datalen;
-	in_port_t myport, port;
-	struct sockaddr_in myskt, skt;
-	unsigned long ipaddr;
-	char sbuf[512], buf[512];
-	int position = 0;
-	socklen_t sktlen;
+        int s; /* File descripter of socket*/
+        int count; /* Byte size of sent data*/
+        int datalen; /* Length of sent data*/
+        in_port_t myport, port;
+        struct sockaddr_in myskt, skt;
+        char sbuf[BUF_SIZE], buf[BUF_SIZE];
+        char *c; /* Used to check input error*/
+        socklen_t sktlen; /* Size of skt*/
 
-	myport = 49151;
-	port = 49152;
-	ipaddr = 13111311053ul;
+        /*Set port*/
+        myport = 12345;
+        port = 12345;
 
-	/*Input data.*/
-    scanf("%s", sbuf);
+        /* Generate socket*/
+        if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+                perror("socket");
+                exit(1);
+        }
 
-	datalen = 512;
+        /* Initialize myskt and allocate socket address*/
+        memset(&myskt, 0, sizeof myskt);
+        myskt.sin_family = AF_INET;
+        myskt.sin_port = htons(myport);
+        myskt.sin_addr.s_addr = INADDR_ANY;
+        if (bind(s, (struct sockaddr *)&myskt, sizeof myskt) < 0) {
+                perror("bind");
+                exit(1);
+        }
 
-	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-			perror("socket");
-			exit(1);
-	}
+        /* Initialize skt address*/
+        memset(&skt, 0, sizeof skt);
+        skt.sin_family = AF_INET;
+        skt.sin_port = htons(port);
+        skt.sin_addr.s_addr = inet_addr(CLI_IP_ADDR);
 
-		memset(&myskt, 0, sizeof myskt);
-		myskt.sin_family = AF_INET;
-		myskt.sin_port = htons(myport);
-		myskt.sin_addr.s_addr = htonl(INADDR_ANY);
-		if (bind(s, (struct sockaddr *)&myskt, sizeof myskt) < 0) {
-			perror("bind");
-			exit(1);
-		}
+        while (1) {
+                /* Initialize sbuf.*/
+                memset(sbuf, 0, sizeof(sbuf));
 
-		memset(&skt, 0, sizeof skt);
-		skt.sin_family = AF_INET;
-		skt.sin_port = htons(port);
-		skt.sin_addr.s_addr = htonl(ipaddr);
-		if ((count = sendto(s, sbuf, datalen, 0,
-							(struct sockaddr *)&skt, sizeof skt)) < 0) {
-				perror("sendto");
-				exit(1);
-			}
-		printf("count = %d", count);
+                /*Input data.*/
+                fprintf(stderr, "When you finish input characters, put Enter\n");
+                if ((c = fgets(sbuf, BUF_SIZE, stdin)) == NULL) {
+                        perror("input");
+                        exit(1);
+                }
 
-		/* Receive*/
-		sktlen = sizeof skt;
-		if ((count = recvfrom(s, buf, sizeof buf, 0,
-							  (struct sockaddr *)&skt, &sktlen)) < 0) {
-			perror("recvfrom");
-			exit(1);
-		}
-		printf("%s\n", buf);
+                datalen = strlen(sbuf);
+
+                /* Send data*/
+                if ((count = sendto(s, sbuf, datalen, 0,
+                                    (struct sockaddr *)&skt, sizeof skt)) < 0) {
+                        /* "count < 0" means error.*/
+                        perror("sendto");
+                        exit(1);
+                }
+
+                printf("Success to send %s (%d byte) to %s\n", sbuf, count,
+                       CLI_IP_ADDR);
+
+                /* Receive data*/
+                sktlen = sizeof skt;
+                memset(buf, 0, sizeof(buf));
+                if ((count = recvfrom(s, buf, (sizeof(char)) * datalen, 0,
+                                      (struct sockaddr *)&skt, &sktlen)) < 0) {
+                        perror("recvfrom");
+                        exit(1);
+                }
+                printf("Return from %s: %s (%d byte)\n", inet_ntoa(skt.sin_addr)
+                       , buf, strlen(buf));
+        }
+
+        close(s);
+
+        return 0;
 }
